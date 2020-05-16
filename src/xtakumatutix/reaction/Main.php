cac9f1f2c9a8da5fcd410f6df37e047c4aa7066b
たくまっち   Takumachi <47268002+xtakumatutix@users.noreply.github.com>
2020-05-16 21:37:35 +0900

    完成(Goodのみ)

--- src/xtakumatutix/reaction/Main.php
+++ src/xtakumatutix/reaction/Main.php
@@ -7,6 +7,7 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\Player;
 use xtakumatutix\reaction\Form\ActionForm;
+use xtakumatutix\reaction\Reaction\Good;
 
 Class Main extends PluginBase 
 {
--- src/xtakumatutix/reaction/Reaction/Good.php
+++ src/xtakumatutix/reaction/Reaction/Good.php
@@ -2,11 +2,32 @@
 namespace xtakumatutix\reaction\Reaction;
 
 use pocketmine\Player;
+use pocketmine\level\Level;
+use pocketmine\math\Vector3;
+use pocketmine\level\particle\FloatingTextParticle;
+use pocketmine\plugin\Plugin;
+use pocketmine\scheduler\ClosureTask;
+use pocketmine\Server;
+
 
 class Good
 {
     public static function particle(Player $player): void
     {
-        $player->sendMessage("ああああああ");
+        $x = $player->getX() + 1;
+        $y = $player->getY() + 1.2;
+        $z = $player->getZ();
+        $level = $player->getLevel();
+        $pos = new Vector3($x, $y, $z);
+        $particle = new FloatingTextParticle($pos, "いいね！！", "§l§eGood!!");
+        $level->addParticle($particle);
+
+        $task = new ClosureTask(function (int $currentTick) use ($particle, $level): void {
+            $particle->setInvisible(true);//パーティクルを見えなくする(これ以外に消す方法あるかも)
+            $level->addParticle($particle);//見えなくしたのを反映させる
+        });
+        $plugin = Server::getInstance()->getPluginManager()->getPlugin("reaction");
+        /** @var Plugin $plugin */
+        $plugin->getScheduler()->scheduleDelayedTask($task, 20 * 5);//5秒後に実行
     }
 }
